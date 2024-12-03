6e7a3bf47de0f00ec27213db1b51956076a93ab6
SireneNouira <sirene.nouira16@gmail.com>
2024-12-03 14:38:40 +0100

    finis

---
+++ EX1/algo.php
@@ -0,0 +1,21 @@
+<?php
+
+// $name = "Prénom";
+// $lastname = "Nouira";
+// $firstname = "Sirène";
+// $age = 21 ;
+// $km = 1;
+// $km = 3;
+// $km = 125;
+// var_dump($km);
+// $string = 'je suis un string';
+// $int = 4;
+// $float = 4.6;
+// $boolean = true;
+// var_dump($string, $int, $float, $boolean);
+// $int;
+// var_dump($int);
+// $int = 4;
+// var_dump($int);
+// $name = "Sirène";
+// echo "<p>Bonjour " . $lastname ." " . $firstname . " tu as " . $age ." ans. </p>";
